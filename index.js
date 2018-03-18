6b907ffc141db9bd146af7c61378dc32f14bcf6d
Trey Briggs <trey.alan.briggs@gmail.com>
2018-03-17 22:35:58 -0600

    thing

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var rateCalc = require('./rateCalc.js');
+const {Client} = require('pg');
+const client = new Client({
+    connectionString: PORT,
+    ssl: true,
+});
+
+client.connect();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/postage'))
-  .get('/rates', rateCalc.calculate)
+  .get('/', (req, res) => res.render('pages/shop'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//.get('/shop/:items', rateCalc.calculate)
+//User req.params.items to use the /shop/items sort of thing
+//to parse a body with express we do .use(express.json()) and .use(express.urlencoded({extended:true}))
---
+++ nodeDBInit.sql
@@ -0,0 +1,35 @@
+drop table if exists public.users, public.items, public.cartitems;
+
+create table public.users (
+    id serial primary key not null,
+    username varchar(256) not null,
+    password varchar(256) not null
+);
+
+create table public.items (
+	id serial primary key not null,
+	name varchar(256) not null,
+	cost decimal(18,2) not null,
+	description text not null
+);
+
+create table public.cartitems (
+	id serial primary key not null,
+	itemid int references items(id),
+	userid int references users(id)
+);
+
+insert into public.users(username, password) values
+	('user1', 'pass1'),
+	('user2', 'pass2'),
+	('user3', 'pass3');
+
+insert into public.items(name, cost, description) values
+	('item1', 20.00, 'this is a description'),
+	('item2', 12.33, 'the second item');
+
+insert into public.cartitems(itemid, userid) values
+	(1,1),
+	(1,2),
+	(2,1),
+	(2,2);
