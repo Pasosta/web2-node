3ec278745453fa44cd019f1b8bdc4fa7f97f9a80
Trey Briggs <trey.alan.briggs@gmail.com>
2018-03-19 17:42:37 -0600

    endpoints

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var shopep = require('shopep.js')
-const {Client} = require('pg');
+var shopep = require('./shopep.js')
+const { Client } = require('pg');
+
 const client = new Client({
-    connectionString: PORT,
-    ssl: true,
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
 client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/shop'))
-  .get('/shop/items', shopep.items),
-  .get('/shop/users', shopep.users),
+  .get('/shop/items', shopep.items)
+  .get('/shop/users', shopep.users)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 //.get('/shop/:items', rateCalc.calculate)
 //User req.params.items to use the /shop/items sort of thing
 //to parse a body with express we do .use(express.json()) and .use(express.urlencoded({extended:true}))
+
+//const express = require('express')
+//const router = express.Router()
+//const path = require('path')
+//const PORT = process.env.PORT || 5000
+//
+//const pg = require('pg')
+//
+
+//const pool = new pg.Pool(config)
+//
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/getPerson', getPerson) 
+//  .get('/', (req, res) => res.render('pages/index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//
+//function getPerson(req, res, next) {
+//    console.log('in the function')
+//    pool.connect(function (err, client, done){
+//        if (err) {
+//            return console.log("Error fetching from pool")
+//        }
+//        console.log('connected to db')
+//        client.query('SELECT * FROM person', function(err, result){
+//            done()
+//            if (err){
+//                return console.error('error running the query', err)
+//            }
+//            res.send(result)
+//        })
+//    })
+//    pool.end()
+//}
--- shopep.js
+++ shopep.js
@@ -1,21 +1,6 @@
-const {Client} = require('pg');
-const client = new Client({
-    connectionString: PORT,
-    ssl: true,
-});
-
-client.connect();
 
 function items(req, res) {
     var itemarray = new Array();
-
-client.query('SELECT * FROM public.items;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    itemarray.push(row)
-  }
-  client.end();
-});
     
     var postageData = {"data": itemarray};
     
@@ -25,14 +10,6 @@ client.query('SELECT * FROM public.items;', (err, res) => {
 function users(req, res) {
     //var item = req.query.item;
 var userarray = new Array();
-
-client.query('SELECT * FROM public.users;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    userarray.push(row)
-  }
-  client.end();
-});
     
     var postageData = {"data": userarray};
     
