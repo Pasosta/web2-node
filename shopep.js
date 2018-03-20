67927736b8ada648c19f6adbd0b219478e294b20
Trey Briggs <trey.alan.briggs@gmail.com>
2018-03-19 21:48:02 -0600

    endpoint

--- shopep.js
+++ shopep.js
@@ -1,5 +1,4 @@
 const { Client } = require('pg');
-const { exec } = require('child_process');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -13,17 +12,16 @@ function items(req, res, next) {
     var itemarray = new Array();
     console.log("items");
     var itemData = ["asd"];
-    client.query('SELECT * FROM public.items;').exec(function (err, res) {
+    client.query('SELECT * FROM public.items;', (err, res) => {
         if (err) throw err;
         for (let row of res.rows) {
             itemarray.push(row);
         }
         itemData = {"data": itemarray};
-        console.log(postageData);
-        res.render('pages/result', itemData);
+        console.log(itemData);
+        res.render('pages/result', itemData)
         client.end();
     });
-    
 }
 
 
