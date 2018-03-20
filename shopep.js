588cfe4d00ba39317c8eb0ce4852409e55067867
Trey Briggs <trey.alan.briggs@gmail.com>
2018-03-20 10:10:20 -0600

    endpoint

--- shopep.js
+++ shopep.js
@@ -12,35 +12,33 @@ client.connect();
 function items(req, response, next) {
     var itemarray = new Array();
     console.log("items");
-    var itemData = ["asd"];
     client.query('SELECT * FROM public.items;', (err, res) => {
         if (err) {
             console.log(err);
         }
         itemData = {"data": res.rows};
         console.log(itemData);
-        //result.render('pages/result', itemData);
-        response.json(res.rows);
+        response.render('pages/result', itemData);
+        //response.json(res.rows);
         client.end();
     });
 }
 
 
-function users(req, res) {
+function users(req, response) {
     //var item = req.query.item;
 var userarray = new Array();
     console.log("user");
-    client.query('SELECT * FROM public.items;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            userarray.push(JSON.stringify(row));
+    client.query('SELECT name FROM public.users;', (err, res) => {
+        if (err) {
+            console.log(err);
         }
+        var userData = {"data": res.rows};
+        console.log(userData);
+        //result.render('pages/result', userData);
+        response.json(res.rows);
         client.end();
     });
-    
-    var userData = {"data": userarray};
-    res.render('pages/result', userData);
-    
     
 }
 
