e47d4d50f6c38052386b7ccb6440db7a5662ef99
Trey Briggs <trey.alan.briggs@gmail.com>
2018-03-22 10:54:09 -0600

    endpoints

--- shopep.js
+++ shopep.js
@@ -1,6 +1,11 @@
 const { Client } = require('pg');
 const { Pool } = require('pg');
-const pool = new Pool();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
 const express = require('express')
 
 const client = new Client({
